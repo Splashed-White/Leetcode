b9ce0f16ecbec1db4ccb914297f6a3e0a1db659b
Splashed-White <214821854@qq.com>
2022-02-08 15:03:02 +0800

    /

--- 剑指offer/机器人的运动范围.java
+++ 剑指offer/机器人的运动范围.java
@@ -8,6 +8,8 @@ package 剑指offer;
  * Time: 3:15
  */
 public class 机器人的运动范围 {
+    private static int count;
+    public static int[][] position = {{1,0},{-1,0},{0,1},{0,-1}};
     public static int add(int value){
         int sum = 0;
         while(value != 0){
@@ -17,19 +19,21 @@ public class 机器人的运动范围 {
         return sum;
     }
     public static int movingCount(int m, int n, int k) {
-        int ret = 0;
-        if(m == 1 && n == 1) return 0;
-        int[][] grid = new int[m-1][n-1];
-        int row = m;
-        int col = n;
-        for(int i = 0; i < row; i++){
-            for(int j = 0; j < col; j++){
-                if(add(i)+add(j) <= k){
-                    ret++;
-                }
-            }
+        boolean[][] book = new boolean[m][n];
+        DFS(m,n,book,0,0,k);
+        return  count;
+    }
+    public static void DFS(int m, int n, boolean[][] book, int sr, int sc, int k){
+        if(sr < 0 || sc < 0 || sr >= m || sc >= n || book[sr][sc] == true) return;
+        if(add(sr) + add(sc) > k) return;
+        book[sr][sc] = true;
+        count++;
+        for(int i = 0; i < 4; i++){
+            int new_sr = sr + position[i][0];
+            int new_sc = sc + position[i][1];
+            DFS(m,n,book,new_sr,new_sc,k);
         }
-        return ret;
+
     }
 
     public static void main(String[] args) {
